45bf6408d498d30d75cbdeda30db6fed3571fb7b
Jeremia Böhmig <26493413+JereB@users.noreply.github.com>
2022-07-10 17:21:15 +0200

    structure parsing of file differently to combine them easier later
    now each file is parsed as book and books can be merged down the line.
    For this purpose all collections are immutable
    datastructures for better performance

--- exporter/src/lib.rs
+++ exporter/src/lib.rs
@@ -1,34 +1,47 @@
-use std::{collections::HashSet, fs::DirEntry, path::Path};
+use std::{fs::DirEntry, path::Path};
 
 use color_eyre::eyre::{eyre, Result, WrapErr};
 use id3::{Tag, TagLike};
+use im::{vector, OrdSet, Vector};
 use serde::Serialize;
 use tracing::warn;
 
-pub fn parse_file<P: AsRef<Path>>(path: P) -> Result<(Track, Tag)> {
+pub fn parse_file<P: AsRef<Path>>(path: P) -> Result<Book> {
     let tag = Tag::read_from_path(&path)
         .wrap_err(format!("can't parse file: {:?}", path.as_ref().display()))?;
     tracing::debug!("read file {:?}", path.as_ref());
 
-    Ok((
-        Track {
-            title: tag
-                .title()
-                .ok_or_else(|| eyre!("no Title defined in File {:?}", path.as_ref()))?
-                .to_string(),
-            reader: tag
-                .artists()
-                .ok_or_else(|| eyre!("No artist defined in File {:?}", path.as_ref()))?
-                .into_iter()
-                .map(String::from)
-                .collect(),
-            track: tag
-                .track()
-                .ok_or_else(|| eyre!("No track defined in {:?}", path.as_ref()))?,
-            disc: tag.disc(),
+    let track = Track {
+        title: tag
+            .title()
+            .ok_or_else(|| eyre!("no Title defined in File {:?}", path.as_ref()))?
+            .to_string(),
+        reader: tag
+            .artists()
+            .ok_or_else(|| eyre!("No artist defined in File {:?}", path.as_ref()))?
+            .into_iter()
+            .map(String::from)
+            .collect(),
+        track: tag
+            .track()
+            .ok_or_else(|| eyre!("No track defined in {:?}", path.as_ref()))?,
+        disc: tag.disc(),
+    };
+
+    Ok(Book {
+        title: track.title.clone(),
+        author: {
+            let author = tag.album_artist().unwrap_or("").to_string();
+            let mut authors = OrdSet::new();
+            authors.insert(author);
+            authors
         },
-        tag,
-    ))
+        reader: track.reader.clone(),
+        tracks: vector![track],
+        total_tracks: 1,
+        discs: tag.total_discs(),
+        year: tag.year(),
+    })
 }
 
 pub fn parse_book<P: AsRef<Path>>(path: P) -> Result<Book> {
@@ -71,25 +84,25 @@ mod test {
 
     #[test]
     fn test_parse_single_file() {
-        let (track, _) = parse_file("../TestData/sherlock_holmes.mp3").unwrap();
-        insta::assert_yaml_snapshot!(track);
+        let book = parse_file("../TestData/sherlock_holmes.mp3").unwrap();
+        insta::assert_yaml_snapshot!(book);
 
-        let (track, _) = parse_file("../TestData/Huckfinn/huckfinn_01_twain_apc_64kb.mp3").unwrap();
-        insta::assert_yaml_snapshot!(track);
+        let book = parse_file("../TestData/Huckfinn/huckfinn_01_twain_apc_64kb.mp3").unwrap();
+        insta::assert_yaml_snapshot!(book);
 
-        let (track, _) =
+        let book =
             parse_file("../TestData/Penguin Island/penguin_island_01_france_64kb.mp3").unwrap();
-        insta::assert_yaml_snapshot!(track);
+        insta::assert_yaml_snapshot!(book);
 
-        let (track, _) = parse_file("../TestData/Winnetou/winnetou1_01_may_64kb.mp3").unwrap();
-        insta::assert_yaml_snapshot!(track);
+        let book = parse_file("../TestData/Winnetou/winnetou1_01_may_64kb.mp3").unwrap();
+        insta::assert_yaml_snapshot!(book);
     }
 }
 
-#[derive(Serialize, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Eq)]
 pub struct Track {
     title: String,
-    reader: Vec<String>,
+    reader: OrdSet<String>,
     track: u32,
     disc: Option<u32>,
 }
@@ -97,11 +110,10 @@ pub struct Track {
 #[derive(Debug, Serialize, PartialEq, Eq)]
 pub struct Book {
     title: String,
-    author: HashSet<String>,
-    reader: HashSet<String>,
-    tracks: Vec<Track>,
+    author: OrdSet<String>,
+    reader: OrdSet<String>,
+    tracks: Vector<Track>,
     total_tracks: u32,
     discs: Option<u32>,
     year: Option<i32>,
-    comments: Vec<String>,
 }
